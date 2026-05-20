163829211514c6544b5af1f58f06736f8cc092f6
dg2026709-byte <dg2026709@danggok.hs.kr>
2026-05-20 14:51:57 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+box()
+sphere()
+scene.lights = []
+l1 = local_light(pos = vec(0, 0, 0), color=color.red)
+b box(pos = vec(0, 0, -1), size=vector(5, 5, 1))
+while True :
+    rate(100)
+    k = keysdown()
+    if = keysdown()
+    if 'left' in k :
+        l1.pos.x-=0.1
+        if 'down' in k  :
+            lpos.x0.1
